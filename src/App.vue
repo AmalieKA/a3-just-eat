49fdbc25d60175c31c2271f4d8721e582445dfb1
AmalieKA <a.ka@live.dk>
2024-10-09 13:56:32 +0200

    1

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,36 @@
 <style scoped>
+    #canvas {
+        width: 390px;
+        height: 844px;
+    }
+
+    #mainscreen {
+        width: 390px;
+        height: 844px;
+        object-fit: cover;
+    }
+
+    #button {
+        position: absolute;
+        top:700px;
+        background-color: #FB6603;
+        border-radius: 100px;
+        width: 370px;
+        height: 45px;
+        margin: 0px 10px;
+        display: flex;
+        gap: 10px;
+    }
 
 </style>
 
 <template>
-
+    <div id="canvas">
+        <img id="mainscreen" src="./assets/mainscreen.PNG">
+        <div id="button">
+            <img src="" alt="">
+        </div>
+    </div>
 </template>
 
 <script setup>
